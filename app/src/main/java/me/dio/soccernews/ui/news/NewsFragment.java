0785e8855df83bee468fcdaa87e4f5cfef94df14
lucaspavani <lucaspavanisantos@outlook.com>
2022-07-13 00:22:22 -0300

    Ajustes no layout

--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import me.dio.soccernews.databinding.FragmentNewsBinding;
+import me.dio.soccernews.ui.adapter.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
@@ -22,7 +24,10 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        newsViewModel.getText().observe(getViewLifecycleOwner(), binding.textNews::setText);
+        binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
+            binding.rvNews.setAdapter(new NewsAdapter(news));
+        });
         return root;
     }
 
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -4,16 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soccernews.domain.News;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+        this.news = new MutableLiveData<>();
+
+        //TODO Remover Mock de Notícias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ferroviária tem desfalque importante.", "Isso é uma descrição. Isso é uma descrição. Isso é uma descrição."));
+        news.add(new News("Ferrinha joga no sábado", "Isso é uma descrição. Isso é uma descrição. Isso é uma descrição."));
+        news.add(new News("Copa do Mundo Feminina está terminando", "Isso é uma descrição. Isso é uma descrição. Isso é uma descrição."));
+
+        this.news.setValue(news);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
